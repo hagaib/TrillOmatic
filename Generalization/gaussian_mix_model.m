cfd1fef049fca699a960a607b72f51c1faa50391
hagaib <hagaibarmatz@gmail.com>
2021-04-27 19:00:00 +0300

    generalization files more comments added

--- Generalization/gaussian_mix_model.m
+++ Generalization/gaussian_mix_model.m
@@ -1,6 +1,13 @@
 function model = gaussian_mix_model(x , points)
-%GAUSSIAN_MIX_MODEL Summary of this function goes here
-%   Detailed explanation goes here
+%GAUSSIAN_MIX_MODEL model function for 1D gaussian mixture model used for
+%nonlinear least squares curve fitting.
+%inputs:
+% x: model parameters [mu1, sigma1, mu2, sigma2, ... muN, sigmaN]
+% points: range of data points (x values).
+%
+% outputs:
+% model: vector of gaussians defined over points build with parameters in x
+
 if(mod(length(x),2) ~= 0)
     error('x must be of length of a multiple of 2');
 end
--- Generalization/gaussian_mix_residual.m
+++ Generalization/gaussian_mix_residual.m
@@ -1,6 +1,15 @@
 function residual = gaussian_mix_residual(x , points , known_val , isplot)
-%GAUSSIAN_MIX_MODEL Summary of this function goes here
-%   Detailed explanation goes here
+% residual function for nonlinear least squares curve fitting using
+% 1D gaussian mixture model.
+%inputs:
+% x: model parameters [mu1, sigma1, mu2, sigma2, ... muN, sigmaN]
+% points: range of data points (x values).
+% known_val: value of data points (y values).
+% is_plot: boolean. toggles plotting of optimization process for debugging
+% purposes
+%
+%outputs:
+% residual: residual to minimize (model - data)
 
 if(nargin<4) , isplot = false; end
 
